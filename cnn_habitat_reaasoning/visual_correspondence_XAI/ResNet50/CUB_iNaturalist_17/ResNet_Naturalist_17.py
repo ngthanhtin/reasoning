6ec70320fa19e24f4c2acef5952bcf0204503389
ngthanhtinqn <ngthanhtinqn@gmail.com>
2023-07-14 14:18:42 -0500

    add two images for doing inference

--- cnn_habitat_reaasoning/visual_correspondence_XAI/ResNet50/CUB_iNaturalist_17/ResNet_Naturalist_17.py
+++ cnn_habitat_reaasoning/visual_correspondence_XAI/ResNet50/CUB_iNaturalist_17/ResNet_Naturalist_17.py
@@ -44,11 +44,39 @@ class ImageFolderWithPaths(ImageFolder):
         
         return (img, label ,path)
 
+class ImageFolderWithTwoPaths(ImageFolder):
+    def __init__(self, root1, root2, transform=None, target_transform=None):
+        super(ImageFolderWithTwoPaths, self).__init__(root1, transform, target_transform)
+        self.root2 = root2
+
+    def __getitem__(self, index):
+  
+        # img, label = super(ImageFolderWithPaths, self).__getitem__(index)
+        
+        # path = self.imgs[index][0]
+        
+        path, label = self.samples[index]
+        img = self.loader(path)
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        path2 = self.root2 +"/" + path.split("/")[-2] + "/" + path.split("/")[-1]
+        img2 = self.loader(path2)
+
+        if self.transform is not None:
+            img2 = self.transform(img2)
+
+        return (img, img2, label, path, path2)
 # %%
 # validation_folder = ImageFolder(root='/home/tin/datasets/cub/CUB/test', transform=val_dataset_transform)
-validation_folder = ImageFolderWithPaths(root='/home/tin/datasets/cub/CUB/test', transform=val_dataset_transform)
-val_loader        = DataLoader(validation_folder, batch_size=512, shuffle=False, num_workers=8, pin_memory=False)
+# validation_folder = ImageFolderWithPaths(root='/home/tin/datasets/cub/CUB/test', transform=val_dataset_transform)
+# val_loader        = DataLoader(validation_folder, batch_size=512, shuffle=False, num_workers=8, pin_memory=False)
 
+
+# %%
+validation_folder = ImageFolderWithTwoPaths(root1='/home/tin/datasets/cub/CUB/test', root2='/home/tin/datasets/cub/CUB_inpaint_all_test/', transform=val_dataset_transform)
+val_loader        = DataLoader(validation_folder, batch_size=512, shuffle=False, num_workers=8, pin_memory=False)
 # %% [markdown]
 # ## iNAT ResNet-50 
 
@@ -123,7 +151,7 @@ habitat_model = timm.create_model(
             in_chans=3,
         )
 # habitat_model = ResNet_AvgPool_classifier(Bottleneck, [3, 4, 6, 4])
-habitat_model_path = '/home/tin/projects/reasoning/cnn_habitat_reaasoning/15_cub_resnet101_0.063.pth'
+habitat_model_path = '/home/tin/projects/reasoning/cnn_habitat_reaasoning/9_cub_resnet101_0.078.pth'
 habitat_model.load_state_dict(torch.load(habitat_model_path))
 habitat_model.to(device)
 
@@ -222,6 +250,48 @@ def test_cub_ensemble_2(id_model, habitat_model, alpha=1):
   return predictions, targets, confidence, paths
 
 # %%
+def test_cub_ensemble_3(id_model, habitat_model, alpha=1):
+  id_model.eval()
+  habitat_model.eval()
+  
+  running_loss = 0.0
+  running_corrects = 0
+  
+  predictions = []
+  targets = []
+  paths = []
+  confidence = []
+  
+  with torch.inference_mode():
+    for _, (data1, data2, target, path, path2) in enumerate(val_loader):
+      data1   = data1.to(device)
+      data2   = data2.to(device)
+      target = target.to(device)
+      
+      id_feat = id_model(data1)
+      habitat_feat = habitat_model(data2)
+      outputs = id_feat*alpha + habitat_feat*(1-alpha)
+      # outputs = outputs.unsqueeze(0)
+
+      loss = criterion(outputs, target)
+      _, preds = torch.max(outputs, 1)
+      probs, _ = torch.max(F.softmax(outputs, dim=1), 1)
+      running_loss += loss.item() * target.size(0)
+      running_corrects += torch.sum(preds == target.data)
+      
+      predictions.extend(preds.data.cpu().numpy())
+      targets.extend(target.data.cpu().numpy())
+      paths.extend(path)
+      confidence.extend((probs.data.cpu().numpy()*100).astype(np.int32))
+
+  epoch_loss = running_loss / len(validation_folder)
+  epoch_acc = running_corrects.double() / len(validation_folder)
+
+  print('-' * 10)
+  print('loss: {:.4f}, acc: {:.4f}'.format(epoch_loss, 100*epoch_acc))
+  
+  return predictions, targets, confidence, paths
+# %%
 # cub_test_ensemble_preds, cub_test_ensemble_targets, cub_test_ensemble_confs, img_paths = test_cub_ensemble_2(inat_resnet, habitat_model, alpha=0.7)
 
 
@@ -257,7 +327,7 @@ def test_cub_ensemble_2(id_model, habitat_model, alpha=1):
 # %%
 for alpha in [0.5, 0.6, 0.7, 0.8, 0.9]:
     print(alpha)
-    cub_test_preds, _, cub_test_confs, _ = test_cub_ensemble_2(inat_resnet, habitat_model, alpha=alpha)
+    cub_test_preds, _, cub_test_confs, _ = test_cub_ensemble_3(inat_resnet, habitat_model, alpha=alpha)
 
 # %%
 
