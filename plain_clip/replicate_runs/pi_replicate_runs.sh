a66df4d9c9ce7246542ad648926fce9d00012320
ngthanhtinqn <ngthanhtinqn@gmail.com>
2024-02-22 16:00:34 -0600

    save class acc

--- plain_clip/replicate_runs/pi_replicate_runs.sh
+++ plain_clip/replicate_runs/pi_replicate_runs.sh
@@ -6,8 +6,8 @@ modes=(
     )
 model_sizes=("ViT-B/32" "ViT-B/16" "ViT-L/14")
 descriptor_paths=(
-    "../descriptors/part_imagenet/78_part_imagenet_descriptions.json"
-    "../descriptors/part_imagenet/78_habitat_part_imagenet_descriptions.json"
+    "../descriptors/part_imagenet/158_part_imagenet_descriptions.json"
+    "../descriptors/part_imagenet/158_habitat_part_imagenet_descriptions.json"
 )
 seeds=(
     5
@@ -20,10 +20,10 @@ seeds=(
 
 for dataset in "${datasets[@]}"; do
     for model_size in "${model_sizes[@]}"; do
-        python ../main.py --dataset="$dataset" --mode="gpt_descriptions" --model_size="$model_size" --descriptor_fnames "${descriptor_paths[@]}" --device cuda:3 --savename="normal_pi_accuracy"
+        python ../main.py --dataset="$dataset" --mode="gpt_descriptions" --save_class_acc --model_size="$model_size" --descriptor_fnames "${descriptor_paths[@]}" --device cuda:0 --savename="158_normal_pi_accuracy"
     done
 
     for model_size in "${model_sizes[@]}"; do
-        python ../main.py --dataset="$dataset" --mode="waffle" --model_size="$model_size" --descriptor_fnames "${descriptor_paths[@]}" --device cuda:3 --savename="waffle_pi_accuracy" --seeds "${seeds[@]}"
+        python ../main.py --dataset="$dataset" --mode="waffle" --save_class_acc --model_size="$model_size" --descriptor_fnames "${descriptor_paths[@]}" --device cuda:0 --savename="158_waffle_pi_accuracy" --seeds "${seeds[@]}"
     done
 done
